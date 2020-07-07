92ab54b64a9a677072ff9ea04aa4f9c22817b729
ranalytica <ranalyticas@gmail.com>
2020-07-07 15:43:09 -0500

    1

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,7 +12,9 @@ app_ui <- function(request) {
     fluidPage(
       h1("Electric Vehicle Investment"),
       mod_01_left_panel_ui("01_left_panel_ui_1"),
-      mod_02_reactive_input_ui("02_reactive_input_ui_1")
+      h3("Reddit Post From WallStreetBets"),
+      mod_02_reactive_input_ui("02_reactive_input_ui_1"),
+      
     )
   )
 }
--- R/mod_02_reactive_input.R
+++ R/mod_02_reactive_input.R
@@ -7,9 +7,17 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
+library(DT)
+library(RedditExtractoR)
+library(dplyr)
+library(lubridate)
+library(gt)
+
 mod_02_reactive_input_ui <- function(id){
   ns <- NS(id)
   tagList(
+   
+    gt_output(ns("tbl"))
     
  
   )
@@ -18,9 +26,50 @@ mod_02_reactive_input_ui <- function(id){
 #' 02_reactive_input Server Function
 #'
 #' @noRd 
-mod_02_reactive_input_server <- function(input, output, session){
+mod_02_reactive_input_server <- function(input, output, session, RedditLinks){
   ns <- session$ns
- 
+  
+RedditLinks <- reactive({ 
+  
+    reddit_urls(
+      search_terms   = glue::glue(input$symb, " Stock"),
+      page_threshold = 1,
+      sort_by = "new",
+      wait_time = 3,
+      subreddit = "wallstreetbets"
+    )
+  })
+  
+RedditLinks1 <- reactive({ 
+  
+  RedditLinks() %>% 
+    mutate(link= paste("[",title,"](",URL,")")
+    )
+  })
+
+RedditDate <- reactive({ 
+  RedditLinks1() %>% mutate(date=dmy(date) 
+                            )
+})
+
+RedditDateA <- reactive({ 
+RedditDate() %>% dplyr::arrange(desc(date))
+  
+})
+link <- reactive({ 
+  RedditDateA() %>% select(date,title)
+})
+
+gt_link <- reactive({   
+   
+link() %>% gt() %>% tab_header(
+  title = glue::glue(input$symb," Stock Reddit post from WallStreetBets forum"))
+  
+  }) 
+
+output$tbl = gt::render_gt(
+  gt_link()
+)
 }
     
 ## To be copied in the UI
